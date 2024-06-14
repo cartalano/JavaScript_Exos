cad565f48f3cf2097a7e09e3670209bf84acc04b
Nicolas Catalano <nicolascatalano1@gmail.com>
2024-06-14 14:37:27 +0200

    MAJ maths

--- 02-maths/04-sort-numbers/script.js
+++ 02-maths/04-sort-numbers/script.js
@@ -9,10 +9,27 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(() => {
-    // to get the value of an input: document.getElementById("element-id").value
+function trierListe(concombre) {
+    for (let i = 0; i<concombre.length; i++) {
+        for (let j=0; j<(concombre.length-1); j++) {
+            if (concombre[j]>concombre[j+1]) {
+                let temp = concombre[j];
+                concombre[j]= concombre[j+1];
+                concombre[j+1]= temp;
+            }
+        }
 
-    document.getElementById("run").addEventListener("click", () => {
-        // your code here
-    });
-})();
+    }
+
+    return concombre; 
+}
+
+function sortNumbers() {
+    let input = document.getElementById("numbers").value;
+     let concombre = input.split(", ").map(Number);
+    let result = trierListe(concombre);
+    alert(result);
+}
+
+document.getElementById("run").addEventListener("click", sortNumbers);
+    
